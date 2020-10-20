1edd58704afaa09e58f23956051b2b1b3095e426
tdofmaxk <58266147+tdofmaxk@users.noreply.github.com>
2020-10-20 02:49:20 +0100

    Create test.go

---
+++ function/test.go
@@ -0,0 +1,20 @@
+package function
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func Handle(w http.ResponseWriter, r *http.Request) {
+	var input []byte
+
+	if r.Body != nil {
+		defer r.Body.Close()
+
+		body, _ := ioutil.ReadAll(r.Body)
+
+		input = body
+	}
+
+	w.WriteHeader(http.StatusOK)
