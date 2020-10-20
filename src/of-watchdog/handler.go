93d17d7d312311e7237990344c6bf025e7d5557d
tdofmaxk <58266147+tdofmaxk@users.noreply.github.com>
2020-10-20 09:06:17 +0100

    test
    fdf

--- src/of-watchdog/handler.go
+++ src/of-watchdog/handler.go
@@ -18,5 +18,5 @@ func Handle(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Hello from OpenFaaS to the App Platform team. Body: %s", string(input))))
+	w.Write([]byte(fmt.Sprintf("Hello from OpenFaaS to the App Platform  %s", string(input))))
 }
